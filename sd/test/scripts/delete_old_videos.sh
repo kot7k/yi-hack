1d59214a6162101c27fa8aaf0164aca096dc6300
kot7k <ilovecps2@hotmail.com>
2016-06-03 16:49:57 +0200

    Update delete_old_videos.sh

--- sd/test/scripts/delete_old_videos.sh
+++ sd/test/scripts/delete_old_videos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #Delete files and folders more than 15 days
 #To run this script you can make an entry the cameras crontab
-#You might need to create manually the folder structure /var/spool/cront/crontabs 
+#You might need to create manually the folder structure /var/spool/cron/crontabs 
 #or when you try to add a crontab with "crontab -e" it might give you an error
 #search on internet about the use of crontab
 
